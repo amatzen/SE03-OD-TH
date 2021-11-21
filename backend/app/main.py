0963ad007ec7d147e355579ca8efa98750a6f58f
Alexander Matzen <alexander@amatzen.dk>
2021-11-21 15:42:35 +0100

    Added backend

---
+++ backend/app/main.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from fastapi import FastAPI, Form
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse, RedirectResponse
+
+from pydantic import BaseModel
+
+# DB
+DATABASE_URL = 'mysql://root:password@database/database'
+
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+
+app = FastAPI()
+
+class Person(Base):
+    __tablename__ = 'people'
+    PersonID = Column(Integer, primary_key=True, autoincrement=True)
+    Firstname = Column(String(255))
+    Lastname = Column(String(255))
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+@app.get('/persons')
+def persons():
+    return JSONResponse(jsonable_encoder(session.query(Person).all()))
+
+
+@app.post('/person')
+def new_person(firstname: str = Form(...), lastname: str = Form(...)):
+    session.add(
+        Person(Firstname = firstname, Lastname = lastname)
+    )
+    session.commit()
+    return RedirectResponse("https://localhost/select.html", status_code=303)
